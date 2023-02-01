b521e1c4aad56dd6d642babbd22cfc77e1467295
muthupandy1332 <124023406+muthupandy1332@users.noreply.github.com>
2023-02-01 18:20:24 +0530

    Add files via upload

---
+++ Manager2.java
@@ -0,0 +1,12 @@
+class Manager2 
+{
+    public static void main (String[] args) 
+    {
+        int num=0;
+
+       num= num++ + num;
+
+        System.out.println(num++);
+        System.out.println(num);
+    }
+}
